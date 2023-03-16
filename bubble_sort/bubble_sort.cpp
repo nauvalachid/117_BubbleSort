1957e7fd57d42e7e34548269769a6ff9c7b7c7ea
nauvalachid <nauvalachid@gmail.com>
2023-03-16 16:15:48 +0700

    Membuka Display

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -36,4 +36,26 @@ void bubbleSortArray() {	// procedur untuk mengurutkan array dengan metode bubbl
 		}
 	}
 }
+
+void unsorted() {
+	cout << endl;							// Output baris kosong
+	cout << "==================================" << endl;	// Output ke layar
+	cout << "Element Array yang belum tersusun" << endl; 		// Output ke layar
+	cout << "==================================" << endl;	// Output ke layar
+	for (int j = 0; j < n; j++) {			// Looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j] << endl;				// Output ke layar
+	}
+	cout << endl;							// Output baris kosong
+}
+
+void display() {	// procedur untuk menampilkan hasil
+	cout << endl;							// Output baris kosong
+	cout << "==================================" << endl;	// Output ke Layar
+	cout << "Element Array yang telah tersusun" << endl;		// Output ke layar	
+	cout << "==================================" << endl;	// Output ke layar
+	for (int j = 0; j < n; j++) {			// Looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j] << endl;				// Output ke layar
+	}
+	cout << endl;							// Output baris kosong
+}
 	
