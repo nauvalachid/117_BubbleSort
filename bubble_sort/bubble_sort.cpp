7bbc96fc02bf2d227fd597adf23a463eecc55253
nauvalachid <nauvalachid@gmail.com>
2023-03-16 15:52:46 +0700

    Membuat Input

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -20,4 +20,20 @@ void input() {			// procedur untuk Input
 	cout << "Masukan Elemen Array" << endl;	// Output ke layar
 	cout << "===================" << endl;	// Output ke layar
 
+	for (int i = 0; i < n; i++) {	// Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": ";	// Output ke layar
+		cin >> a[i];					// Input dari pengguna
+	}
+}
+void bubbleSortArray() {	// procedur untuk mengurutkan array dengan metode bubble sort
+	for (int i = 1; i < n; i++) {		// Looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j < n - 1; j++) {
+			if (a[j] > a[j + 1]) {			// Jika nilai pada a[j] lebih besar dari a[j+1]
+				int temp = a[j];			// Simpan nilai a[j] ke variabel sementara tetap
+				a[j] = a[j + 1];			// Assign nilai a [j+1] ke a[j]
+				a[j + 1] = temp;			// Assign nilai temp ke a [j+1]
+			}
+		}
+	}
+}
 	
